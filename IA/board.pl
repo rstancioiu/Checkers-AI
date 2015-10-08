4091ea8922790fea1b38495d1984255e563333c9
Benoit Chabod <be.chabod@gmail.com>
2015-10-08 16:23:12 +0200

    Clean IA methods and add dynamic board

--- IA/board.pl
+++ IA/board.pl
@@ -1,3 +1,4 @@
+:- dynamic board/1.
 board([[0,1,0,1,0,1,0,1,0,1],
 			 [1,0,1,0,1,0,1,0,1,0],
 			 [0,1,0,1,0,1,0,1,0,1],
@@ -14,24 +15,35 @@ game :-
 	write(' Prolog Checkers    '),nl,
 	write('********************'),nl,
 	board(B),
-	show1(B).
-	
-show1([L1,L2,L3,L4,L5,L6,L7,L8,L9,L10]) :-
-	show2(L1),nl,show2(L2),nl,show2(L3),nl,show2(L4),nl,show2(L5),nl,show2(L6),nl,show2(L7),nl,show2(L8),nl,show2(L9),nl,show2(L10),nl.
+	show_board(B).
 
-show2([L1,L2,L3,L4,L5,L6,L7,L8,L9,L10]) :-
+show_board([L1,L2,L3,L4,L5,L6,L7,L8,L9,L10]) :-
+	show_line(L1),nl,show_line(L2),nl,show_line(L3),nl,show_line(L4),nl,show_line(L5),nl,show_line(L6),nl,show_line(L7),nl,show_line(L8),nl,show_line(L9),nl,show_line(L10),nl.
+
+show_line([L1,L2,L3,L4,L5,L6,L7,L8,L9,L10]) :-
 	write(L1),write(L2),write(L3),write(L4),write(L5),write(L6),write(L7),write(L8),write(L9),write(L10).
 
-replace([_|T], 0, X, [X|T]) :- !.
-replace([H|T], I, X, [H|R]):- I > 0, NI is I-1, replace(T, NI, X, R).
+%set(Array, Indice, New element, New array ?)
+set([_|T], 0, X, [X|T]) :- !.
+set([H|T], I, X, [H|R]):- I > 0, NI is I-1, set(T, NI, X, R).
 
-move1(OLDX,OLDY,X,Y) :-
+%get2D(2DArray, IndiceX, IndiceY, Value ?)
+get2D(B,X,Y,VAL) :-
+	nth0(Y, B, LINE),
+	nth0(X, LINE, VAL).
+
+%set2D(2DArray, IndiceX, IndiceY, New Element, New 2DArray ?)
+set2D(B,X,Y,VAL,B2) :-
+	nth0(Y, B, LINE),
+	set(LINE, X, VAL, LINE2),
+	set(B, Y, LINE2, B2).
+	
+move(OLDX,OLDY,X,Y) :-
 	board(B),
-	nth0(OLDY, B, LINE),
-	nth0(OLDX, LINE, VAL),
-	replace(LINE, OLDX, 0, LINE2),
-	replace(B, OLDY, LINE2, NEWB),
-	nth0(Y, NEWB, NEWLINE),
-	replace(NEWLINE, X, VAL, NEWLINE2),
-	replace(NEWB, Y, NEWLINE2, NEWB2),
-	show1(NEWB2).
+	%Backup old value, and set it to 0
+	get2D(B,OLDX,OLDY,VAL),
+	set2D(B,OLDX,OLDY,0,B2),
+	%Set the value at new position
+	set2D(B2,X,Y,VAL,B3),
+	retract(board(_)),
+	assert(board(B3)).
