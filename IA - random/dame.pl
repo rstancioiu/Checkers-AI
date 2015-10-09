ae2b768a87f83eb3af5baccb9c5f54de933bb6ef
hugoSip <hugo.sipahi@hotmail.fr>
2015-10-09 14:14:01 +0200

    squash! essai de resolution du probleme aleablanc (pas fini, à retester)

--- IA - random/dame.pl
+++ IA - random/dame.pl
@@ -14,17 +14,15 @@ posBlanc([[1,1],[1,3],[3,1],[3,3],[5,1],[5,3],[7,1],[7,3],[9,1],[9,3],[2,2],[2,4
 posNoir([[5,5],[7,7],[9,7]]).
 pionBlanc(X,Y):- posBlanc(L),member([X,Y],L).
 pionNoir(X,Y):- posNoir(L),member([X,Y],L).
-clean(1):- retract(posBlanc(_)),retract(posNoir(_)),asserta(posNoir([[1,7],[1,9],[3,7],[3,9],[5,7],[5,9],[7,7],[7,9],[9,7],[9,9],[2,8],[2,10],[4,8],[4,10],[6,8],[6,10],[8,8],[8,10],[10,8],[10,10]])),asserta(posBlanc([[1,1],[1,3],[3,1],[3,3],[5,1],[5,3],[7,1],[7,3],[9,1],[9,3],[2,2],[2,4],[4,2],[4,4],[6,2],[6,4],[8,2],[8,4],[10,2],[10,4]])).
+%clean(1):- retract(posBlanc(_)),retract(posNoir(_)),asserta(posNoir([[1,7],[1,9],[3,7],[3,9],[5,7],[5,9],[7,7],[7,9],[9,7],[9,9],[2,8],[2,10],[4,8],[4,10],[6,8],[6,10],[8,8],[8,10],[10,8],[10,10]])),asserta(posBlanc([[1,1],[1,3],[3,1],[3,3],[5,1],[5,3],[7,1],[7,3],[9,1],[9,3],[2,2],[2,4],[4,2],[4,4],[6,2],[6,4],[8,2],[8,4],[10,2],[10,4]])).
 
 %--DEPLACEMENT ALEATOIRE--
 randum(X):- X is random(3)-1.
 randum(X):-randum(X),X\==0.
 
 %--Deplacement Blanc--
-aleaBlanc(X2,Y2,X,Y,1):- aleaBlancBouffer(X2,Y2,X,Y),moveBlanc(X2,Y2,X,Y).
-aleaBlanc(X3,Y3,X2,Y2,2):- aleaBlanc(X2,Y2,_,_,1),aleaBlancBouffer(X3,Y3,X2,Y2),moveBlanc(X3,Y3,X2,Y2).
-aleaBlanc(X3,Y3,X2,Y2,3):- aleaBlanc(X2,Y2,_,_,2),aleaBlancBouffer(X3,Y3,X2,Y2),moveBlanc(X3,Y3,X2,Y2).
-aleaBlanc(X2,Y2,X,Y,0):- not(aleaBlanc(X2,Y2,X,Y,I),I > 0,not(aleaBlancBouffer(X2,Y2,X,Y)),aleaBlancPasBouffer(X2,Y2,X,Y),moveBlanc(X2,Y2,X,Y),!.
+%aleaBlanc(X2,Y2,X,Y,jeBouffePas):- not(aleaBlancBouffer(X2,Y2,X,Y)),aleaBlancPasBouffer(X2,Y2,X,Y),moveBlanc(X2,Y2,X,Y),!.
+aleaBlanc(X2,Y2,X,Y,jeBouffe):- aleaBlancBouffer(X2,Y2,X,Y),moveBlanc(X2,Y2,X,Y).
 
 aleaBlancPasBouffer(X2,Y2,X,Y):- randum(Xi),pionBlanc(X,Y),Y2 is Y + 1,X2 is X + Xi,damier(X2,Y2),not(pionBlanc(X2,Y2)),not(pionNoir(X2,Y2)),!.
 aleaBlancBouffer(X2,Y2,X,Y):- Xi is 1,Xi2 is Xi + Xi,pionBlanc(X,Y),Xn is X + Xi, Yn is Y + 1,pionNoir(Xn,Yn),Y2 is Y + 2,X2 is X + Xi2,damier(X2,Y2),not(pionBlanc(X2,Y2)),not(pionNoir(X2,Y2)),supprimeNoir(Xn,Yn),!.
